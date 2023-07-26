53fd2de5537a0bdeedf3acd5f99d2edb953b905c
Gaye Colakoglu <55553433+gayecolakoglu@users.noreply.github.com>
2023-07-26 17:09:44 +0300

    asal sayıları ekrana yazdıran program

---
+++ asal.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Asal Sayılar: ");
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    // Metod to check if a number is prime
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

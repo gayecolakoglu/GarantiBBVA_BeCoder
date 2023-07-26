a3fb85460f0da97c643c4a83773cd0208a6e9e81
Gaye Colakoglu <55553433+gayecolakoglu@users.noreply.github.com>
2023-07-26 17:01:02 +0300

    yıldızlar ile elmas

---
+++ yildizlar.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz (Tek Sayı Olmalı) :");
+        int n = klavye.nextInt();
+
+        // Üst yarısını oluşturuyoruz
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Alt yarısını oluşturuyoruz
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

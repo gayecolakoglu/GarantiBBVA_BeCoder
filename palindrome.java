cbc32ba1a213700294937c6eaf5daa68df559179
Gaye Colakoglu <55553433+gayecolakoglu@users.noreply.github.com>
2023-07-29 16:28:28 +0300

    palindromik kelimeleri bulan program

---
+++ palindrome.java
@@ -0,0 +1,21 @@
+public class Main {
+    static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String word = "abba";
+        if (isPalindrome(word)) {
+            System.out.println(word + " kelimesi palindromiktir.");
+        } else {
+            System.out.println(word + " kelimesi palindromik değildir.");
+        }
+    }
+}
